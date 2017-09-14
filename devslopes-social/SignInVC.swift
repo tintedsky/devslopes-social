6c6cf77c9c3ffb45928ee614bfdf74cc847e6ef6
tintedsky <hongbonew@gmail.com>
2017-09-14 17:25:17 -0400

    facebook login authentication completed

--- devslopes-social/SignInVC.swift
+++ devslopes-social/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,31 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil{
+                print("JESSHB: Unable to authenticate with Facebook - \(String(describing: error))")
+            }else if result?.isCancelled == true{
+                print("JESSHB: User cancelled Facebook authentication")
+            }else{
+                print("JESSHB: Successfully authenticated with Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential){
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if error != nil {
+                print("JESSHB: Unable to authenticate with Firebase - \(String(describing: error))")
+            } else{
+                print("JESSHB: Successfully authenticated with Firebase")
+            }
+        }
+    }
 
 }
 

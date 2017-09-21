017c22cd271fa1f0b310b34fa8ae6b89e4f973b8
tintedsky <hongbonew@gmail.com>
2017-09-20 21:55:03 -0400

    Completed allowing user to select a post image

--- devslopes-social/FeedVC.swift
+++ devslopes-social/FeedVC.swift
@@ -10,17 +10,23 @@ import UIKit
 import SwiftKeychainWrapper
 import Firebase
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
    
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageAddIcon: CircleView!
     
     var posts = [Post]()
+    var imagePicker:UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
         
         // Do any additional setup after loading the view.
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
@@ -55,7 +61,6 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell{
             cell.configureCell(post: post)
             return cell
-            
         }else{
             return PostCell()
         }
@@ -68,4 +73,17 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         dismiss(animated: true, completion: nil)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //Hongbo: Here we need to pay attention to, to get the selected image from imagePicker
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage{
+            imageAddIcon.image = image
+        }else{
+            print("JESSHB: A valid image wasn't selected")
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addImageTapped(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
